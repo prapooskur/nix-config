c2bc6f5806765d1387ffb6ef692411b309bd5095
Prasiddh Pooskur <61389919+prapooskur@users.noreply.github.com>
2025-11-06 23:47:03 -0800

    Update system.nix

--- modules/system.nix
+++ modules/system.nix
@@ -3,138 +3,144 @@
 
   system = {
 
-    # desktop/windowing
-    defaults.WindowManager = {
-      EnableStandardClickToShowDesktop = false;
-      # disable tiling (handled by rectangle)
-      EnableTiledWindowMargins = false;
-      EnableTilingByEdgeDrag = false;
-      EnableTilingOptionAccelerator = false;
-      EnableTopTilingByEdgeDrag = false;
-      StandardHideDesktopIcons = true;
-      StandardHideWidgets = false;
-    };
-    # hide desktop icons, show widgets
-    defaults.finder.CreateDesktop = false;
-    defaults.controlcenter.NowPlaying = true;
-
-    # dock
-    defaults.dock = {
-      autohide = true;
-      autohide-delay = 0.08; # default 0.24
-      autohide-time-modifier = 0.66; # default 1.0
-    };
-
-    # fix insane finder defaults
-    defaults.finder.QuitMenuItem = true;
-    defaults.finder.ShowPathbar = true;
-    defaults.finder._FXShowPosixPathInTitle = true;
-
-    # input devices
-    # trackpad
-    defaults.trackpad.FirstClickThreshold = 2; # firm
-    # keyboard
+    # keyboard (why isn't this in defaults)
     keyboard = {
       enableKeyMapping = true;
       remapCapsLockToControl = true;
       swapLeftCtrlAndFn = true;
     };
-    defaults.hitoolbox.AppleFnUsageType = "Do Nothing";
-    # fix insane autocorrect defaults
-    defaults.NSGlobalDomain = {
-      NSAutomaticSpellingCorrectionEnabled = false;
-      NSAutomaticQuoteSubstitutionEnabled = false;
-      NSAutomaticPeriodSubstitutionEnabled = false;
-      NSAutomaticInlinePredictionEnabled = false;
-      NSAutomaticDashSubstitutionEnabled = false;
-      NSAutomaticCapitalizationEnabled = false;
-      ApplePressAndHoldEnabled = false;
-    };
 
-    # key shortcuts
-    defaults.CustomUserPreferences = {
-      "com.apple.symbolichotkeys" = {
-        AppleSymbolicHotKeys = {
+    defaults = {
+      # desktop/windowing
+      WindowManager = {
+        EnableStandardClickToShowDesktop = false;
+        # disable tiling (handled by rectangle)
+        EnableTiledWindowMargins = false;
+        EnableTilingByEdgeDrag = false;
+        EnableTilingOptionAccelerator = false;
+        EnableTopTilingByEdgeDrag = false;
+        StandardHideDesktopIcons = true;
+        StandardHideWidgets = false;
+      };
+      # hide desktop icons, show widgets
+      finder.CreateDesktop = false;
+      # show now playing widget in top bar
+      controlcenter.NowPlaying = true;
 
-          # disable dock autohide toggle
-          "52" = {
-            enabled = false;
-            value = {
-              parameters = [
-                100
-                2
-                1572864
-              ];
-              type = "standard";
+      # dock
+      dock = {
+        autohide = true;
+        autohide-delay = 0.08; # default 0.24
+        autohide-time-modifier = 0.66; # default 1.0
+      };
+
+      # fix insane finder defaults
+      finder = {
+        QuitMenuItem = true;
+        ShowPathbar = true;
+        _FXShowPosixPathInTitle = true;
+      };
+
+      # trackpad
+      trackpad.FirstClickThreshold = 2; # firm
+      # disable emoji popup
+      hitoolbox.AppleFnUsageType = "Do Nothing";
+      # fix insane autocorrect defaults
+      NSGlobalDomain = {
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticInlinePredictionEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticCapitalizationEnabled = false;
+        ApplePressAndHoldEnabled = false;
+      };
+
+      # key shortcuts
+      CustomUserPreferences = {
+        "com.apple.symbolichotkeys" = {
+          AppleSymbolicHotKeys = {
+
+            # disable dock autohide toggle
+            "52" = {
+              enabled = false;
+              value = {
+                parameters = [
+                  100
+                  2
+                  1572864
+                ];
+                type = "standard";
+              };
             };
-          };
 
-          # disable stage manager toggle
-          "222" = {
-            enabled = false;
-            value = {
-              parameters = [
-                65535
-                65535
-                0
-              ];
-              type = "standard";
+            # disable stage manager toggle
+            "222" = {
+              enabled = false;
+              value = {
+                parameters = [
+                  65535
+                  65535
+                  0
+                ];
+                type = "standard";
+              };
             };
-          };
 
-          # Disable: Move focus to next window (Cmd + `) - alttab handles this for us (keep default cmd-tab)
-          "27" = {
-            enabled = false;
-            value = {
-              parameters = [
-                96
-                50
-                1048576
-              ];
-              type = "standard";
+            # Disable: Move focus to next window (Cmd + `) - alttab handles this for us (keep default cmd-tab)
+            "27" = {
+              enabled = false;
+              value = {
+                parameters = [
+                  96
+                  50
+                  1048576
+                ];
+                type = "standard";
+              };
             };
-          };
 
-          # Disable F11 to show desktop
-          "36" = {
-            enabled = false;
-            value = {
-              parameters = [
-                65535
-                103
-                8388608
-              ];
-              type = "standard";
+            # Disable F11 to show desktop
+            "36" = {
+              enabled = false;
+              value = {
+                parameters = [
+                  65535
+                  103
+                  8388608
+                ];
+                type = "standard";
+              };
             };
-          };
 
-          # Save picture of selected area as a file (Cmd + Shift + S)
-          "31" = {
-            enabled = true;
-            value = {
-              parameters = [
-                115
-                1
-                1179648
-              ];
-              type = "standard";
+            # Save picture of selected area as a file (Cmd + Shift + S)
+            "31" = {
+              enabled = true;
+              value = {
+                parameters = [
+                  115
+                  1
+                  1179648
+                ];
+                type = "standard";
+              };
             };
-          };
-          # Disable 'Cmd + Space' for Spotlight Search
-          "64" = {
-            enabled = false;
-          };
+            # Disable 'Cmd + Space' for Spotlight Search
+            "64" = {
+              enabled = false;
+            };
 
-          # disable quick note on fn+q
-          "190" = {
-            enabled = false;
-            value = {
-              parameters = [
-                113
-                12
-                8388608
-              ];
-              type = "standard";
+            # disable quick note on fn+q
+            "190" = {
+              enabled = false;
+              value = {
+                parameters = [
+                  113
+                  12
+                  8388608
+                ];
+                type = "standard";
+              };
             };
           };
         };
